63861a90f840f01deda63a68d6168304bbe72066
Maria Belen Molina <beel18molina@gmail.com>
2023-11-01 00:00:23 -0300

    Ejercicio 3 completo

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function ejercicio1() {alert("un mensaje");}
 
 // Ejercicio 2
 function ejercicio2() {document.write("Hello World");}
+
+// Ejercicio 3
+function ejercicio3() {
+    let resultado = 3 + 5;
+    document.write(resultado);
+}
